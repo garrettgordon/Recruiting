967b0fbfa080739c8a7a1ab0420bc150818500b6
Vinit Nayak <vinitnayak87@berkeley.edu>
2014-03-12 16:53:26 -0700

    Created Users scaffolding, added register link to index.html.erb directing to new user registration. Made password hidden. Added (but currently commented out) logic for handling user sessions.

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'coffee-rails', '~> 4.0.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 
+# Use authLogic for user authentication
+gem 'authlogic'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312012957) do
+ActiveRecord::Schema.define(version: 20140312205606) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 20140312012957) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
