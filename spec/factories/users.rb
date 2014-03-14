81e82be8c83b505ec8a6ad1a7a16dd3073eeb417
Eric O'Neill <ericoneill10@gmail.com>
2014-03-14 12:21:02 -0700

    Added some user tests
    User creation and retrieval
    mostly committing because I need to pull to update

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-# spec/factories/users.rb
-
-require 'faker'
-FactoryGirl.define do
-	factory :users do |f|
-		f.username { Faker::Internet.user_name }
-		f.email { Faker::Internet.email }
-	end
-end
