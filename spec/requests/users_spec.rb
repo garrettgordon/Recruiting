80fbe6f89af421ebbb786b102d9249ad40c30dc0
Eric O'Neill <ericoneill10@gmail.com>
2014-04-02 11:40:15 -0700

    sucessful functional test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,6 @@
 require 'spec_helper'
+require 'factory_girl_rails'
+
 
 describe "Users" do
   describe "GET /users" do
@@ -8,4 +10,21 @@ describe "Users" do
       response.status.should be(200)
     end
   end
+
+  describe "has profile page" do
+  	it "has logout on profile page" do
+  		sign_in()
+  		current_path.should == "/home"
+  	end
+
+  end
+  def sign_in()
+  	@user1 = FactoryGirl.create(:user)
+  	visit "/"
+  	fill_in "user_session_username", with: "john"
+  	fill_in "user_session_password", with: "pass"
+  	click_button "Login"
+  end
+
+
 end
