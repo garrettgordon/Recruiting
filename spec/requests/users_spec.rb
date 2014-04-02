58f6e7e244e1dca02cba1f8895846749f8c72434
Eric O'Neill <ericoneill10@gmail.com>
2014-04-02 13:55:29 -0700

    more tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,15 +11,51 @@ describe "Users" do
     end
   end
 
-  describe "has profile page" do
-  	it "has logout on profile page" do
+  describe "logon" do
+  	it "logs in with succesful login" do
+  		@user1 = FactoryGirl.create(:user)
   		sign_in()
   		current_path.should == "/home"
   	end
 
+  	it "doesn't login with incorrect parameters" do
+  		@user1 = FactoryGirl.create(:user)
+	  	visit "/"
+	  	fill_in "user_session_username", with: "john"
+	  	fill_in "user_session_password", with: "pass1"
+	  	click_button "Login"
+	  	current_path.should == "/"
+  	end
+
   end
-  def sign_in()
-  	@user1 = FactoryGirl.create(:user)
+
+  describe "home page" do
+  	it "goes to profile page" do
+  		@user1 = FactoryGirl.create(:user)
+  		sign_in()
+  		click_link(@user1.username)
+  		current_path.should == "/users/1"
+  	end
+
+  	it "logs out" do
+  		@user1 = FactoryGirl.create(:user)
+  		sign_in()
+  		click_link("Logout")
+  		current_path.should == "/"
+  	end
+
+  	it "goes to event" do
+  		@user1 = FactoryGirl.create(:user)
+  		@event1 = FactoryGirl.create(:event)
+  		sign_in()
+  		click_link("Details")
+  		current_path.should == "/events/1"
+
+  	end
+
+  end
+
+  def sign_in()	
   	visit "/"
   	fill_in "user_session_username", with: "john"
   	fill_in "user_session_password", with: "pass"
