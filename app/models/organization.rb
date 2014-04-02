2bb14f86eb67b1163302fe7d08e2d0c25fb1e97b
garrettgordon <garrettgordon@berkeley.edu>
2014-04-02 13:31:37 -0700

    organiztion model testing

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ActiveRecord::Base
 	has_and_belongs_to_many :events
 	has_and_belongs_to_many :users
+	MAX_NAME_LENGTH = 128
+	validates :name, :presence => true, :length => {maximum: MAX_NAME_LENGTH}
 end
