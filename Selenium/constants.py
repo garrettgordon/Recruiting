b70c00e6d4030ccc535de0a6c9d2d4ddd2aab012
Vinit Nayak <vinitnayak87@berkeley.edu>
2014-04-01 17:19:09 -0700

    Added user login UI test. Change USERNAME and PASSWORD constants in constants.py to a valid user on the server you are testing on.

--- Selenium/constants.py
+++ Selenium/constants.py
@@ -26,9 +26,15 @@ HOMEPAGE = 'http://localhost:3000'
 #HOMEPAGE = 'http://mysterious-woodland-3202.herokuapp.com/'
 HOMEPAGE_STRING = 'BERKELEY'
 HOMEPAGE_HEADER = "UPLOAD. APPLY"
+HOMEPAGE_LOGIN_ID = "user_session_username"
+HOMEPAGE_PASSWORD_ID = "user_session_password"
 HOMEPAGE_STRING_XPATH = "//*[@id='page-nav-about']/div/h1"
 HOMEPAGE_BROWSE_SIGN_UP = '//*[@id="page-nav-contact"]/div/div/div[2]/h1'
 
+USERNAME = "vinit"
+PASSWORD = "nine"
+LOGIN_BUTTON_XPATH = '//*[@id="new_user_session"]/input[3]'
+NAVBAR_USER_NAME_XPATH = '/html/body/div[1]/div/div/div[2]/a[2]'
 
 """------------------------------------------------------------------------------------------------------------------
 		THANK YOU PAGE
---
+++ Selenium/otc_4.py
@@ -0,0 +1,55 @@
+"""
+Developer           - Vinit Nayak
+Maintainer          - ""
+Version             - v1.0
+Date                - June 2012
+Test Case ID        - otc_4
+Test Description    - '****To check - User Login' """
+
+from constants import *
+from testconfiguration import *
+from lib import *
+
+class otc_4(unittest.TestCase):
+    
+    def setUp(self):
+        initiate_setup(self)
+        self.resultFile.write('****To check - Entry Points into MP3 Retail Store\n')
+        self.resultFile.write("Test Areas - Login\n")
+
+    def test_entry_via_drop_down(self):
+        ## WARNING: This assumes that there exists a user with USERNAME and PASSWORD
+        wd_handle = self.wd_handle
+        resultFile = self.resultFile
+        wd_handle.get(HOMEPAGE)
+        try:
+            username_textbox = wd_handle.find_element_by_id(HOMEPAGE_LOGIN_ID)
+            username_textbox.send_keys(USERNAME)
+        except:
+            resultFile.write(TEST_FAILED + "Failed to input username\n")
+    
+        try:
+            password_textbox = wd_handle.find_element_by_id(HOMEPAGE_PASSWORD_ID)
+            password_textbox.send_keys(PASSWORD)
+        except:
+            resultFile.write(TEST_FAILED + "Failed to input password\n")
+
+        try:
+            login_button = wd_handle.find_element_by_xpath(LOGIN_BUTTON_XPATH)
+            login_button.click()
+        except:
+            resultFile.write(TEST_FAILED + "Login Button click failed\n")
+
+        try:
+            navbar_name = wd_handle.find_element_by_xpath(NAVBAR_USER_NAME_XPATH)
+            if navbar_name.text == USERNAME:
+                resultFile.write(TEST_PASSED + " LOGIN SUCCESSFUL\n")
+        except Exception, e:
+            resultFile.write(TEST_FAILED + "LOGIN UNSUCCESSFUL \n")
+
+    def tearDown(self):
+        self.filename = __file__
+        initiate_teardown(self)
+
+if __name__ == "__main__":
+    unittest.main()
