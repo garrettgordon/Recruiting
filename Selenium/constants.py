27982e841b8d28006e80f3bd4df78984b14629ec
Vinit Nayak <vinitnayak87@berkeley.edu>
2014-04-18 13:16:06 -0700

    Added user sign up test.

--- Selenium/constants.py
+++ Selenium/constants.py
@@ -30,8 +30,17 @@ HOMEPAGE_PASSWORD_ID = "user_session_password"
 HOMEPAGE_STRING_XPATH = "//*[@id='page-nav-about']/div/h1"
 HOMEPAGE_BROWSE_SIGN_UP = '//*[@id="page-nav-contact"]/div/div/div[2]/h1'
 
+STUDENT_SIGN_UP_BUTTON = '//*[@id="new_user"]/div[2]/input'
+STUDENT_USERNAME_TEXTBOX = '//*[@id="user_username"]'
+STUDENT_EMAIL_TEXTBOX = '//*[@id="user_email"]'
+STUDENT_PASSWORD_TEXTBOX = '//*[@id="user_password"]'
+STUDENT_PASSWORD_CONFIRM_TEXTBOX = '//*[@id="user_password_confirmation"]'
+EMAIL_STRING_XPATH = '/html/body/div[2]/center/h2'
+EMAIL_STRING = "Please check your email for your email confirmation"
+
 USERNAME = "vinit"
 PASSWORD = "nine"
+EMAIL_ADDRESS = 'vinitnayak87@berkeley.edu'
 LOGIN_BUTTON_XPATH = '//*[@id="new_user_session"]/input[3]'
 NAVBAR_USER_NAME_XPATH = '/html/body/div[1]/div/div/div[2]/a[2]'
 
