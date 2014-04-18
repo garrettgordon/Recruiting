cb19eddaf474b5fd39dda2201c9bfdf3b11a3d33
garrettgordon <garrettgordon@berkeley.edu>
2014-04-18 16:24:38 -0700

    functional tests

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -48,6 +48,54 @@ describe "Organizations" do
 
   	end
 
+    it "has job listings" do
+      @user1 = FactoryGirl.create(:user) 
+      @org1 = FactoryGirl.build(:organization)
+      @org1.name = "abcd"
+      @org1.save
+      @job1= FactoryGirl.build(:job)
+      @job1.organization=@org1
+      @job2= FactoryGirl.build(:job)
+      @job2.organization=@org1
+      @job1.save
+      @job2.save
+      @user1.save
+      @org1.save
+      sign_in()
+      visit "/organizations/1"
+      page.should have_content("Job Listings:")
+      page.should have_content("MyString")
+    end
+
+    it "has add jobs link if recruiter" do
+      @user1 = FactoryGirl.create(:user, :username => "Paul", :password => "pass", :recruiter => true)
+      @user2 = FactoryGirl.create(:user, :username => "Kyle", :email => "blue@berkeley.edu", :password => "pass", :recruiter => false) 
+      @org1 = FactoryGirl.build(:organization)
+      @user1.organizations << @org1
+      @org1.name = "abcd"
+      @org1.save
+      @user1.save
+      @user2.save
+      visit "/"
+      fill_in "user_session_username", with: "Paul"
+      fill_in "user_session_password", with: "pass"
+      click_button "Login"
+      visit "/organizations/1"
+      page.should have_content("Add Job")
+      click_link "Add Job"
+      current_path.should == '/jobs/new'
+      visit "/"
+      click_link "Logout"
+
+      visit "/"
+      fill_in "user_session_username", with: "Kyle"
+      fill_in "user_session_password", with: "pass"
+      click_button "Login"
+      visit "/organizations/1"
+      page.should_not have_content("Add Job")
+
+    end
+
 
 
   	def sign_in()	
